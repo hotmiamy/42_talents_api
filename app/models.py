ac9a6c5d0f1911291fa3bf21547fba04f4096573
lucas lopes <llopes-n@localhost.localdomain>
2025-03-23 22:24:03 -0300

    add table name

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Profile(db.Model):
+    __tablename__ = 'profiles'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
